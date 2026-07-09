b0d89c3ec019565b26b07da2356d0ee86e0024e8
barbara <rodriguessbarbara@hotmail.com>
2026-07-09 20:30:12 -0300

    chore: add spring dependnecy

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,12 +22,15 @@ repositories {
 
 dependencies {
 	// Configurações de Inicialização e Infra
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-flyway")
 	implementation("org.springframework.boot:spring-boot-starter-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-amqp")
 	// Banco de Dados e Kotlin
 	implementation("org.flywaydb:flyway-database-postgresql")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
